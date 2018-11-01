76942a078ac7483a9f7d13dc5fa1c87c51431247
Niko Kokkinos <niko.kokkinos@outlook.com>
2018-11-01 13:27:41 +0100

    project initialized

---
+++ AttributesProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AttributesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
